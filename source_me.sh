4dc20e4cbbf8bf02ea0381d2f9c80d78b659d54d
Ted Xie <ted.xie@virginia.edu>
2017-10-11 13:01:38 -0400

    modify installation script

--- source_me.sh
+++ source_me.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 export REAPR_HOME=$PWD
+# create a virtual env if one doesn't exist already
+if [ ! -d "virt" ]; then
+    virtualenv virt
+    source virt/bin/activate
+    # install jinja2 for template rendering
+    pip install jinja2
+fi
