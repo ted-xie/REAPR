29eb1f1c91e4003cc675c54c77fcf831920f94b3
Ted Xie <ted.xie@virginia.edu>
2017-10-13 16:17:54 -0400

    add custom reapr_python alias that points to virtual environment

--- source_me.sh
+++ source_me.sh
@@ -6,4 +6,10 @@ if [ ! -d "virt" ]; then
     source virt/bin/activate
     # install jinja2 for template rendering
     pip install jinja2
+
+    # exit the virtual environment
+    deactivate
 fi
+# make a custom command for REAPR's python installation
+alias reapr_python="$REAPR_HOME/virt/bin/python3"
+
